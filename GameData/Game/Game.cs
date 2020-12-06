15542df8e8e72b35d51c9943b9ee01b5ad6864db
Marek Sowa <Marek.Sowa@ibm.com>
2020-12-06 14:30:47 +0100

    game turn

--- GameData/Game/Game.cs
+++ GameData/Game/Game.cs
@@ -12,6 +12,7 @@ namespace MerchantGameConsole.GameData.Game
         public int Difficulty { get; set; }
         public Player Player { get; set; }
         public World World { get; set; }
+        public int Turn { get; set; }
 
         private Notification Notification;
         private GameController GameController;
@@ -21,6 +22,7 @@ namespace MerchantGameConsole.GameData.Game
             this.Player = Player;
             this.World = World;
             this.Difficulty = Difficulty;
+            this.Turn = 1;
             Notification = new Notification();
             GameController = new GameController();
         }

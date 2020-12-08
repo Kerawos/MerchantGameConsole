5cf405ed03d88b05706fa1a94b068e977994d472
Marek Sowa <Marek.Sowa@ibm.com>
2020-12-08 21:17:09 +0100

    player create player

--- GameData/PlayerCharacter/Player.cs
+++ GameData/PlayerCharacter/Player.cs
@@ -9,11 +9,11 @@ namespace MerchantGameConsole.GameData.PlayerCharacter
 {
     public class Player
     {
-        private readonly int Money_Starting = 100;
+        private readonly int MoneyStarting = 100;
         public string Name { get; set; }
         public int Money { get; set; }
         public List<Item> Items { get; set; }
-        public int Score { get; set; }
+        private int Score { get; set; }
 
         public Player(string name)
         {
@@ -22,9 +22,22 @@ namespace MerchantGameConsole.GameData.PlayerCharacter
 
         public Player (string name, double gameDifficulty) : this(name)
         {
-            this.Money = (int)(Money_Starting * gameDifficulty);
-            this.Items.Add(new Weapon().Create(gameDifficulty));
-            
+            this.Money = (int)(MoneyStarting * gameDifficulty);
+            this.Items = new List<Item>() {
+                new Weapon().Create(gameDifficulty),
+                new Weapon().Create(gameDifficulty),
+                new Weapon().Create(gameDifficulty)};
+        }
+
+        public int GetScore()
+        {
+            ScoreUpdate();
+            return Score;
+        }
+
+        private void ScoreUpdate()
+        {
+            this.Score = Money + Items.Count() * MoneyStarting / 2;
         }
 
 

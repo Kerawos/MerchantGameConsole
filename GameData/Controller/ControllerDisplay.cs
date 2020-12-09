de267741fd4b282c0062f92bb531d4190a095f59
Marek Sowa <Marek.Sowa@ibm.com>
2020-12-09 21:22:22 +0100

    fix response from console

--- GameData/Controller/ControllerDisplay.cs
+++ GameData/Controller/ControllerDisplay.cs
@@ -25,25 +25,36 @@ namespace MerchantGameConsole.GameData.Controller
             return Console.ReadLine();
         }
 
+        public void ViewClear()
+        {
+            Console.Clear();
+        }
+
         public void ProgramLaunch()
         {
+            ViewClear();
             DisplayToUser(N.GameLaunch() + "\r\n" + N.PressAnyKey());
             GetResponseFromUser();
         }
 
         public void ProgramExit()
         {
+            ViewClear();
             DisplayToUser(N.GameExit() + "\r\n" + N.PressAnyKey());
+            GetResponseFromUser();
         }
 
         public void MainMenu()
         {
+            ViewClear();
             DisplayToUser(N.MainMenuContext() + "\r\n" + N.PressAnyKey());
         }
 
         public void GameAbout()
         {
+            ViewClear();
             DisplayToUser(N.GameAbout() + "\r\n" + N.PressAnyKey());
+            GetResponseFromUser();
         }
 
 

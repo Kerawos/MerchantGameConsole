f57ee7c5638a8bfbcaaafab14a6dc680f096c977
Marek Sowa <Marek.Sowa@ibm.com>
2020-12-10 19:47:21 +0100

    weapon test

---
+++ GameTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MerchantGameConsole.GameData.GameConent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantGameConsole.GameData.GameConent.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void testAddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
